f1521a0e90870f780f1afa706f1b866b0ef87816
Phil@Laverne <phil@hanlin.com.tw>
2021-09-26 18:16:58 +0800

    remove comments

--- src/main/java/com/hanlin/crusher/controller/MeshController.java
+++ src/main/java/com/hanlin/crusher/controller/MeshController.java
@@ -37,13 +37,6 @@ public class MeshController {
         var filename = "my-test.zip";
         var contentHeaderValue = String.format(CONTENT_HEADER_FORMAT, filename);
 
-//        Map<String, String> keyValues = new HashMap<String, String>() {{
-//            put("734.jpeg", "https://i.guim.co.uk/img/media/fe12f65605d563fef03ac97336034c67be449354/0_10_7360_4414/master/7360.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=ca94f6d0dfa8c0bc925d5a4c62b7596c");
-//            put("a.jpeg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvvtN3RqRevksgJli80I1eO8N5mW-UcAYm4Q&usqp=CAU");
-//            put("test2.doc", "https://resource.hle.com.tw/Books/BooksResource/110%E4%B8%8A%E9%AB%98%E4%B8%AD%E8%8B%B1%E6%96%87%28%E4%B8%80%29%E6%95%99%E5%AD%B8PPT-L1%28110f342560%29.pptx");
-//            put("test.doc", "https://resource.hle.com.tw/Books/BooksResource/110%E4%B8%8A%E9%AB%98%E4%B8%AD%E8%8B%B1%E6%96%87%28%E4%B8%80%29%E6%95%99%E5%AD%B8PPT-L6%28110f342570%29.pptx");
-//        }};
-
         Map<String, String> keyValues = new HashMap<String, String>() {{
             put("1.pptx", "110上高中英文(一)教學PPT-L7(110f342572).pptx");
             put("2.pptx", "110上高中英文(一)教學PPT-L9(110f342576).pptx");
